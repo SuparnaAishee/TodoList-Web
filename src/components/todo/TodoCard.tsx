955f906900fbe3c9a2bd5dd9cafbed1072ec09d4
SuparnaAishee <suparnad806Qgmail.com>
2024-07-02 21:01:55 +0600

    task done/pending part

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,23 +1,47 @@
-import { useAppDispatch } from "@/redux/hook";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 import { Button } from "../ui/button";
-import { removeTodo } from "@/redux/features/todoSlice";
+import { removeTodo, toggleComplete } from "@/redux/features/todoSlice";
 export type TTodoCardProps ={
   id:string,
   title:string;
   description:string;
+  isCompleted?:boolean;
 }
 
-const TodoCard=({title,description,id}:TTodoCardProps)=>{
+const TodoCard=({title,description,id,isCompleted}:TTodoCardProps)=>{
   const dispatch = useAppDispatch();
+    const task = useAppSelector((state) =>
+      state.todos.todos.find((item: { id: string; }) => item.id === id)
+    );
+
+  const toggleState=()=>{
+  dispatch(toggleComplete(id));
+  };
+    if (!task) return null;
     return (
       <div className="bg-white rounded-md flex justify-between items-center p-3 border">
-        <input type="checkbox" name="" id=""></input>
-          <p className="font-semibold">{title}</p>
+        <input
+          type="checkbox"
+          checked={task.isCompleted}
+          onChange={toggleState}
+          name="complete"
+          id={`complete-${id}`}
+        ></input>
+        <p className="font-semibold">{title}</p>
         {/* <p>Time</p> */}
+        <div>
+          {isCompleted ? (
+            <p className="text-green-500">Done</p>
+          ) : (
+            <p className="text-red-500">Pending</p>
+          )}
+        </div>
         <p>{description}</p>
         <div className="space-x-5">
-          <Button onClick={()=>dispatch(removeTodo(id))}
-          className="bg-red-500">
+          <Button
+            onClick={() => dispatch(removeTodo(id))}
+            className="bg-red-500"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="size-5"
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -18,13 +18,32 @@ const todoSlice=createSlice({
 initialState,
 reducers:{
     addTodo:(state,action:PayloadAction<TTdo>)=>{
-state.todos.push({...action.payload,isCompleted:false});
+      state.todos.push({ ...action.payload, isCompleted: false });
+      // Sort the array to ensure new tasks are before completed tasks
+      state.todos.sort((a, b) => {
+        if (a.isCompleted === b.isCompleted) {
+          return 0;
+        }
+        return a.isCompleted ? 1 : -1;
+      });
     },
     removeTodo:(state,action:PayloadAction<string>)=>{
      state.todos=   state.todos.filter((item)=>item.id !==action.payload);
-    }
+    },
+    toggleComplete:(state,action:PayloadAction<string>)=>{
+      const task = state.todos.findIndex((item) => item.id === action.payload);
+     
+       
+      if (task !== -1) {
+        // Toggle the completion status of the task
+        state.todos[task].isCompleted = !state.todos[task].isCompleted;
+ state.todos = state.todos.sort(
+   (a, b) => Number(a.isCompleted) - Number(b.isCompleted)
+ );
+      }
+    },
 },
 })
 
-export const{addTodo,removeTodo}=todoSlice.actions;
+export const{addTodo,removeTodo,toggleComplete}=todoSlice.actions;
 export default todoSlice.reducer;
